e715c718d7fa4f08245b491ace1d4afdfea1946e
Rajivs1 <rajeev04632@gmail.com>
2024-09-09 14:36:29 +0530

    fitness exercise web app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className='flex items-center px-10 md:px-20 py-10 md:pt-0 md:pb-5 bg-slate-900'>
       <div className='w-full md:w-1/2 md:mt-16'>
         <h1 className='text-2xl md:px-4 text-center md:text-start md:text-4xl lg:text-5xl xl:text-7xl bg-gradient-to-r from-slate-300 to-red-500 bg-clip-text text-transparent tracking-tight font-bold uppercase'>empower your body, <p>energize your mind</p></h1>
-        <p className='text-center sm:text-start text-slate-300 text-xs sm:text-sm xl:text-lg p-5 font-medium italic capitalize'>fuel your fitness revolution with fitness monster.</p>
+        <p className='text-center sm:text-start text-slate-300 text-xs sm:text-sm xl:text-lg p-5 font-medium italic capitalize'>fuel your fitness revolution with FITPro..</p>
         <div className='flex items-center justify-center md:justify-start'>
           <button className='bg-red-600 hover:bg-red-500 my-3 lg:my-5 md:mx-4 md:my-0 lg:mx-5 px-6 py-2 lg:py-3 text-sm lg:text-lg font-semibold italic text-white capitalize rounded-md'>join club</button>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <div className='flex items-center justify-between px-10 py-3 bg-slate-900 text-slate-200'>
       <Link to='/' className='flex items-center justify-center'>
         <img src={logo} alt="logo" className='w-8 md:w-10 rounded-full' />
-        <h2 className='hidden md:flex text-2xl font-bold p-2 italic'>Fitness Monster</h2>
+        <h2 className='hidden md:flex text-2xl font-bold p-2 italic'>FITPro</h2>
       </Link>
       <div className='md:mr-14'>
         <Link to='/' className='text-xs md:text-xl p-1 md:p-2 mx-1 md:mx-3 font-medium underline md:tracking-wide'>Home</Link>
